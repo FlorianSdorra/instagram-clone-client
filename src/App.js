8cb26519e3633a2b5fd905dedc37c7f29debe2b9
FlorianSdorra <fsd114@posteo.de>
2020-12-04 19:07:28 +0100

    links in Nav work, showing different screens when clkicked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import NavBar from './components/Navbar.js'
-
+import NavBar from './components/Navbar.js';
+import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/screens/Home.js';
+import Signup from './components/screens/Signup.js';
+import Profile from './components/screens/Profile.js';
+import Login from './components/screens/Login.js';
 
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
+      <BrowserRouter>
+        <NavBar/>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/signup">
+          <Signup/>
+        </Route>
+        <Route path="/profile">
+          <Profile/>
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
