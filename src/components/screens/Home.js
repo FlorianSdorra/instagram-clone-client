2267fcb636351e7df772465f629674e271053707
FlorianSdorra <fsd114@posteo.de>
2020-12-14 20:28:16 +0100

    styled home screen

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,7 +2,46 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>User name</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2000&q=80" alt="cardimage"/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons ">favorite_border</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>User name</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2000&q=80" alt="cardimage"/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons ">favorite_border</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>User name</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2000&q=80" alt="cardimage"/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" >favorite_border</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
