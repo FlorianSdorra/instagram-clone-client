6061e15098b4c148957e02da8a0fd310c4a483c8
FlorianSdorra <fsd114@posteo.de>
2020-12-14 16:05:56 +0100

    styled login & signup screen

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2 className="mybrand-logo">Instagram</h2>
+                <input
+                type="text"
+                placeholder="email"
+                />
+                <input
+                type="text"
+                placeholder="password"
+                />
+                <button className="btn waves-effect waves-light #2979ff blue accent-2">Log In</button>
+                <h5>
+                    Don't have an Account? <Link to="/signup" className="myLink"> Sign Up</Link>
+                </h5>
+            </div>
+        </div>
     )
 }
 
--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <h1>Signup</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2 className="mybrand-logo">Instagram</h2>
+                <input
+                type="text"
+                placeholder="name"
+                />
+                <input
+                type="text"
+                placeholder="email"
+                />
+                <input
+                type="text"
+                placeholder="password"
+                />
+                <button className="btn waves-effect waves-light #2979ff blue accent-2">Sign Up</button>
+                <h5>
+                    Have an Account? <Link to="/login" className="myLink"> Log In</Link>
+                </h5>
+            </div>
+        </div>
     )
 }
 
