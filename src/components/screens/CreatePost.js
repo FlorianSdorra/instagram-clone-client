629daba114e09bb82be387bd56e162726844e352
FlorianSdorra <fsd114@posteo.de>
2021-01-13 22:05:44 +0100

    connecting client side with cloud to upload pic in createPost

--- src/components/screens/CreatePost.js
+++ src/components/screens/CreatePost.js
@@ -1,6 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+import secret from '../../keys';
+import {useHistory} from 'react-router-dom';
+import M from 'materialize-css';
+const {CLOUD_NAME, UPLOAD_PRESET} = secret;
+
 
 const CreatePost = () =>{
+    const history = useHistory();
+     const [title, setTitle] = useState("");
+     const [body, setBody] = useState("");
+     const [image, setImage] = useState("");
+     const [url, setUrl] = useState("")
+
+     const postDetails = () =>{
+         const data = new FormData()
+         data.append("file", image)
+         data.append("upload_preset", UPLOAD_PRESET )
+         data.append("cloud_name", CLOUD_NAME)
+         fetch(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,{
+             method:"post",
+             body:data
+         })
+         .then(res=>res.json())
+         .then(data=>{
+             setUrl(data.url)
+         })
+         .catch(err=>{
+             console.log(err)
+         })
+         fetch("/createPost",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                title,
+                body,
+                pic:url
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.error){
+                M.toast({html: data.error, classes:"#c62828 red darken"})
+            }
+            else{
+                M.toast({html: "Logged In successfully", classes:"#43a047 green darken-1"});
+                history.push('/')
+            }
+        }).catch(err=>{
+            console.log(err)
+        })   
+     }
     return(
         <div className="card input-field"
         style={{
@@ -9,18 +60,35 @@ const CreatePost = () =>{
             padding:"30px",
             textAlign:"center"
         }}>
-            <input type="text" placeholder="title"/>
-            <input type="text" placeholder="body"/>
+            <input 
+            type="text"
+            placeholder="title"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
+            />
+            <input
+            type="text"
+            placeholder="body"
+            value={body}
+            onChange={(e)=>setBody(e.target.value)}
+            />
             <div className="file-field input-field">
                 <div className="btn #2979ff blue accent-2">
                     <span>Upload Image</span>
-                    <input type="file"/>
+                    <input
+                    type="file"
+                    onChange={(e)=>setImage(e.target.files[0])}
+                    />
                 </div>
                 <div className="file-path-wrapper">
                     <input className="file-path validate" type="text"/> 
                 </div>
                 </div>
-                <button className="btn waves-effect waves-light #2979ff blue accent-2">Submit Post</button>   
+                <button
+                className="btn waves-effect waves-light #2979ff blue accent-2"
+                onClick={()=> postDetails()}
+                >Submit Post
+                </button>   
         </div>
     )
 }
