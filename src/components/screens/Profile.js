23f253620807328da4a8a439548a5a1e60d823d5
FlorianSdorra <fsd114@posteo.de>
2021-06-07 14:34:13 +0200

    little changes for dynamic profile

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -31,10 +31,11 @@ const Profile = () => {
                 </div>
                 <div>
                     <h4>{state?state.name:"loading"}</h4>
+                    <h5>{state?state.email:"loading"}</h5>
                     <div style={{display:"flex", justifyContent:"space-between", width:"109%"}}>
-                        <h6>40 posts</h6>
-                        <h6>40 followers</h6>
-                        <h6>40 following</h6>
+                        <h6>{mypics.length} posts</h6>
+                        <h6>{state.followers.length}followers</h6>
+                        <h6>{state.following.length} following</h6>
                     </div>
                 </div>    
             </div>
